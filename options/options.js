8ffb59803c1f6990738c6fe804bed2cba2cd3db1
Crowdbotics <team@crowdbotics.com>
2021-08-24 15:20:22 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "wispy_darkness_30071Identifier",
+  url: "https://wispy_darkness_30071Identifier.botics.co",
+  api: "https://wispy_darkness_30071Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
